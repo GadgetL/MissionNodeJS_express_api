2418cea6dadbc1c8f69512f54f152cdd9de1eb15
GadgetL <laura.ramirezcosmes@gmail.com>
2022-05-11 15:24:54 -0500

    Create endpoint that GET explorer

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //usando objeto express
 const express = require('express')
 const app = express() //app de express
-app.use(express.json) //indicamos que usaremos JSON
+app.use(express.json()) //indicamos que usaremos JSON
 const port = 3000 //puerto de la app
 
 //HTTP methods
 app.get('/v1/explorers', (req, res) => {
-    console.log(`Api Explorers GET ALL requests ${newDate()}`)
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
     const explorer1 = { id: 1, name: 'GadgetL1' }
     const explorer2 = { id: 2, name: 'GadgetL2' }
     const explorer3 = { id: 3, name: 'GadgetL3' }
@@ -15,9 +15,14 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:d', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = { id: 1, name: 'GadgetL' }
+    res.status(200).json(explorer)
+})
+
 //inicializando la app
 app.listen(port, () => {
-    console.log(`
-                    Example aa listening on port $ { port }
-                    `)
+    console.log(`Example a listening on port ${port}`)
 })
